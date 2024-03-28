f7938d8647548f072666ab28330932d3ff85624f
DMAND <dmandxist@gmail.com>
2024-03-28 16:53:27 +0300

    Create IOrder.cs

---
+++ Server/Interfaces/IOrder.cs
@@ -0,0 +1,12 @@
+﻿using BlazorIntus2.Shared.DataModel;
+namespace BlazorIntus2.Server.Interfaces
+{
+	public interface IOrder
+	{
+		public List<Data.Order> GetOrdersDetails();
+		public void AddOrder(Data.Order orders);		
+		public Data.Order GetOrderData(int id);
+		public void UpdateOrder(Data.Order orders);
+		public void DeleteOrder(int id);
+	}
+}
