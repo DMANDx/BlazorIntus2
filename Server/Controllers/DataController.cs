67a034ac89c2ac3673e39f4aa3949ca85ced3522
DMAND <dmandxist@gmail.com>
2024-03-29 15:46:44 +0300

    Update DataController.cs

--- Server/Controllers/DataController.cs
+++ Server/Controllers/DataController.cs
@@ -102,10 +102,9 @@ namespace BlazorIntus2.Server.Controllers
         }
 
 
-        //Test for list
-        [HttpPost]
-        [Route("api/Data/UpdateOrderWindows")]
-        public async Task<IActionResult> UpdateOrderWindows([FromBody] UpdateOrderWindowsModel  model)
+        //Test for list        
+        [HttpPost("/api/Data/UpdateOrderWindows")]
+        public async Task<IActionResult> UpdateOrderWindows([FromBody] UpdateOrderWindowsModel  model)        
         {
             foreach (var window in model.SelectedWindows)
             {
